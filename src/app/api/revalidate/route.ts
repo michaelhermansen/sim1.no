f328ff7a83fede47cd6c0e1d6ef6d1c1acdd1c09
Michael Hermansen <michael.p.hermansen@gmail.com>
2023-12-11 12:35:58 +0100

    fix: revalidate response

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,6 +1,7 @@
 import { revalidatePath } from "next/cache";
+import { NextResponse } from "next/server";
 
 export async function POST() {
   revalidatePath("/");
-  return Response.json({ status: "ok" });
+  return NextResponse.json({ status: "Ok" }, { status: 200 });
 }
